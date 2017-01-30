f224462f04099f68c8113659917da5e3f001799b
Arturo Rinaldi <arty.net2@gmail.com>
2017-01-30 23:52:19 +0100

    improving clean_all.sh script

--- clean_all.sh
+++ clean_all.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-for i in libusb-1.0.20 dfu-util-0.9 libusb-1.0.21 eudev-3.1.5 hidapi
+list="dfu-util-0.8 dfu-util-0.9 libusb-1.0.9 libusb-1.0.20 libusb-1.0.21 eudev-3.1.5 hidapi OpenOCD"
+
+for i in $list
 do
     cd $i
     make distclean
     cd ..
 done
+
+git clean -f -d
