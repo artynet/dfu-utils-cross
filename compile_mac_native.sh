f2dc86af017ad0adb2323cd21f610ecde6db4931
Arturo Rinaldi <arty.net2@gmail.com>
2017-01-30 12:59:53 +0100

    fixing mac native script to build against static libusb

--- compile_mac_native.sh
+++ compile_mac_native.sh
@@ -15,19 +15,23 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+export CFLAGS="-arch x86_64 -mmacosx-version-min=10.5"
+export CXXFLAGS="-arch x86_64 -mmacosx-version-min=10.5"
+export LDFLAGS="-arch x86_64"
+
 mkdir -p distrib/osx
 cd libusb-1.0.21
-./bootstrap.sh
 export LIBUSB_DIR=`pwd`
+./bootstrap.sh
 ./configure --enable-static --disable-shared
 make clean
 make
 cd ..
 
 cd dfu-util-0.9
-USB_CFLAGS="-I$LIBUSB_DIR/libusb/" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0" ./configure
+USB_CFLAGS="-I$LIBUSB_DIR/libusb/ -framework IOKit -framework CoreFoundation" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0" ./configure
 make clean
-CFLAGS=-static make
+CFLAGS="${CFLAGS} -static" make
 cp src/dfu-suffix src/dfu-prefix src/dfu-util ../distrib/osx/
 #cd libusb-1.0.9 && make distclean && cd..
 #cd dfu-util-0.8 && make distclean && cd..
