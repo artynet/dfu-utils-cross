94e1d9afaf8269d84af1f2234c70a2810bb0a993
Arturo Rinaldi <arty.net2@gmail.com>
2017-02-01 13:02:30 +0100

    adding bash script to build dfu-util trunk

--- clean_all.sh
+++ clean_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-list="dfu-util-0.8 dfu-util-0.9 libusb-1.0.9 libusb-1.0.20 libusb-1.0.21 eudev-3.1.5 hidapi OpenOCD"
+list="dfu-util-0.8 dfu-util-0.9 libusb-1.0.9 libusb-1.0.20 libusb-1.0.21 \
+    dfu-util-official eudev-3.1.5 hidapi OpenOCD"
 
 for i in $list
 do
