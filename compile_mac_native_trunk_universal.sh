a26a81bb71aafd5058189478509270c71889e113
Arturo Rinaldi <arty.net2@gmail.com>
2017-02-01 13:14:44 +0100

    adding bash script to build dfu-util trunk (universal binary)

---
+++ compile_mac_native_trunk_universal.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -xe
+# Copyright (c) 2016 Arduino LLC
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+export CFLAGS="-arch x86_64 -arch i386 -mmacosx-version-min=10.5"
+export CXXFLAGS="-arch x86_64 -arch i386 -mmacosx-version-min=10.5"
+export LDFLAGS="-arch x86_64 -arch i386"
+
+mkdir -p distrib/osx
+cd libusb-1.0.21
+export LIBUSB_DIR=`pwd`
+./configure --enable-static --disable-shared
+make clean
+make
+cd ..
+
+cd dfu-util-official
+./autogen.sh
+USB_CFLAGS="-I$LIBUSB_DIR/libusb/ -framework IOKit -framework CoreFoundation" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0" ./configure
+make clean
+CFLAGS="${CFLAGS} -static" make
+cp src/dfu-suffix src/dfu-prefix src/dfu-util ../distrib/osx/
+#cd libusb-1.0.9 && make distclean && cd..
+#cd dfu-util-0.8 && make distclean && cd..
